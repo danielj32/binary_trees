a97f185dec462bdcfbc5df04a32ab1d9cbe2ca0b
danielj32 <danieljuanito21@hotmail.com>
2020-07-22 02:20:50 +0000

    fix add 2

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -8,10 +8,7 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 {
 	if (node == NULL)
 		return (0);
-
-	if (node->right == NULL)
-		return (1);
-	else if (node->left == NULL)
+	if (node->right == NULL && node->left == NULL)
 		return (1);
 	return (0);
 }
